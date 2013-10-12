aec38132c2071abfd47102706bc790592820e546
springning <ustcnsp@gmail.com>
2013-10-12 11:17:45 +0800

    xiugaistring

--- src/main/java/com/nsp/test/jdk/lang/StringTest.java
+++ src/main/java/com/nsp/test/jdk/lang/StringTest.java
@@ -4,6 +4,7 @@ package com.nsp.test.jdk.lang;
 import java.util.regex.Pattern;
 
 import org.apache.commons.lang3.SystemUtils;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.google.common.base.Splitter;
@@ -15,6 +16,7 @@ public class StringTest
 	private static final Pattern OPERATOR_PATTERN = Pattern.compile(OPERATOR_STRING_REG_EXP);
 	
 	@Test
+	@Ignore
 	public void testSplit()
 	{
 		String testStr = "(asd+bfe)/(123+456)-123*4";
@@ -27,6 +29,18 @@ public class StringTest
 		Iterable<String> splitedStringIterable = Splitter.on(OPERATOR_PATTERN).omitEmptyStrings().split(testStr);
 		for (String string : splitedStringIterable) {
 			System.out.println(string);
+		}
+	}
+	
+	
+	@Test
+	public void testToCharArray()
+	{
+		String test = "宁思平a1";
+		char[] charArray = test.toCharArray();
+		
+		for (char c : charArray) {
+			System.out.println(c);
 		}
 	}
 }
